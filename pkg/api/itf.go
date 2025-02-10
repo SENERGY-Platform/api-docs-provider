0feac3f72cbd865b6fe93c0cced2367719901f43
y-du <42994541+y-du@users.noreply.github.com>
2025-02-10 08:56:29 +0100

    update interface

--- pkg/api/itf.go
+++ pkg/api/itf.go
@@ -3,8 +3,11 @@ package api
 import (
 	"context"
 	"encoding/json"
+	srv_info_lib "github.com/SENERGY-Platform/go-service-base/srv-info-hdl/lib"
 )
 
 type Service interface {
 	GetSwaggerDocs(ctx context.Context, userRoles []string) ([]map[string]json.RawMessage, error)
+	HealthCheck(ctx context.Context) error
+	GetSrvInfo(_ context.Context) srv_info_lib.SrvInfo
 }
