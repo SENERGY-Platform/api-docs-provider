112760b21c335773124773271ecd9ad15ff01b33
y-du <42994541+y-du@users.noreply.github.com>
2025-02-11 08:48:47 +0100

    refactor

--- pkg/service/filter.go
+++ pkg/service/filter.go
@@ -42,32 +42,25 @@ func (s *Service) filterDoc(ctx context.Context, doc map[string]json.RawMessage,
 func (s *Service) getNewPathsByRoles(ctx context.Context, oldPaths map[string]map[string]json.RawMessage, basePath string, userRoles []string) (map[string]map[string]json.RawMessage, error) {
 	newPaths := make(map[string]map[string]json.RawMessage)
 	for subPath, methods := range oldPaths {
-		allowedMethods, err := s.getAllowedMethodsByRoles(ctx, path.Join(basePath, subPath), userRoles, methods)
-		if err != nil {
-			return nil, err
+		allowedMethods := make(map[string]json.RawMessage)
+		fullPath := path.Join(basePath, subPath)
+		for method, rawMessage := range methods {
+			for _, role := range userRoles {
+				ok, err := s.getAccessPolicyByRole(ctx, fullPath, role, method)
+				if err != nil {
+					return nil, err
+				}
+				if ok {
+					allowedMethods[method] = rawMessage
+					break
+				}
+			}
 		}
 		if len(allowedMethods) > 0 {
 			newPaths[subPath] = allowedMethods
 		}
 	}
 	return newPaths, nil
-}
-
-func (s *Service) getAllowedMethodsByRoles(ctx context.Context, fullPath string, userRoles []string, methods map[string]json.RawMessage) (map[string]json.RawMessage, error) {
-	allowedMethods := make(map[string]json.RawMessage)
-	for method, rawMessage := range methods {
-		for _, role := range userRoles {
-			ok, err := s.getAccessPolicyByRole(ctx, fullPath, role, method)
-			if err != nil {
-				return nil, err
-			}
-			if ok {
-				allowedMethods[method] = rawMessage
-				break
-			}
-		}
-	}
-	return allowedMethods, nil
 }
 
 func (s *Service) getAccessPolicyByRole(ctx context.Context, fullPath, role, method string) (bool, error) {
