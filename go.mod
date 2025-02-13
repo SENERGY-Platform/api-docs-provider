3a4eb599f540213be05e357443c964b974be85d0
y-du <42994541+y-du@users.noreply.github.com>
2025-02-13 09:36:36 +0100

    switch to senergy env-loader

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.23
 require (
 	github.com/SENERGY-Platform/gin-middleware v0.5.1
 	github.com/SENERGY-Platform/go-base-http-client v0.1.0
-	github.com/SENERGY-Platform/go-service-base/config-hdl v0.1.1
+	github.com/SENERGY-Platform/go-env-loader v0.5.3
+	github.com/SENERGY-Platform/go-service-base/config-hdl v0.2.0
 	github.com/SENERGY-Platform/go-service-base/logger v0.2.0
 	github.com/SENERGY-Platform/go-service-base/srv-info-hdl v0.0.3
 	github.com/SENERGY-Platform/go-service-base/srv-info-hdl/lib v0.0.2
@@ -13,7 +14,6 @@ require (
 	github.com/gin-contrib/requestid v1.0.4
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
-	github.com/y-du/go-env-loader v0.5.2
 	github.com/y-du/go-log-level v1.0.0
 )
 
--- pkg/config/parser.go
+++ pkg/config/parser.go
@@ -1,22 +1,16 @@
 package config
 
 import (
+	envldr "github.com/SENERGY-Platform/go-env-loader"
 	sb_logger "github.com/SENERGY-Platform/go-service-base/logger"
-	envldr "github.com/y-du/go-env-loader"
 	"github.com/y-du/go-log-level/level"
 	"reflect"
 	"strings"
-	"time"
 )
 
 var typeParser = map[reflect.Type]envldr.Parser{
-	reflect.TypeOf(level.Off):       sb_logger.LevelParser,
-	reflect.TypeOf(time.Nanosecond): durationParser,
-	reflect.TypeOf([]string{}):      listParser,
-}
-
-func durationParser(_ reflect.Type, val string, _ []string, _ map[string]string) (interface{}, error) {
-	return time.ParseDuration(val)
+	reflect.TypeOf(level.Off):  sb_logger.LevelParser,
+	reflect.TypeOf([]string{}): listParser,
 }
 
 func listParser(_ reflect.Type, val string, _ []string, kwParams map[string]string) (interface{}, error) {
