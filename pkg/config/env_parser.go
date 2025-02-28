ef71adb677ecff29b3c56270fb5b80d1cc15bacd
y-du <42994541+y-du@users.noreply.github.com>
2025-02-28 13:04:47 +0100

    refactor

--- pkg/config/env_parser.go
+++ pkg/config/env_parser.go
@@ -2,20 +2,11 @@ package config
 
 import (
 	config_hdl "github.com/SENERGY-Platform/go-service-base/config-hdl"
-	config_env_parser "github.com/SENERGY-Platform/go-service-base/config-hdl/env_parser"
-	config_types "github.com/SENERGY-Platform/go-service-base/config-hdl/types"
 	sb_logger "github.com/SENERGY-Platform/go-service-base/logger"
 	"github.com/y-du/go-log-level/level"
 	"reflect"
 	"strings"
 )
-
-var envTypeParser = []config_hdl.EnvTypeParser{
-	config_types.SecretEnvTypeParser,
-	config_env_parser.DurationEnvTypeParser,
-	logLevelEnvTypeParser,
-	listEnvTypeParser,
-}
 
 func logLevelEnvTypeParser() (reflect.Type, config_hdl.EnvParser) {
 	return reflect.TypeOf(level.Off), sb_logger.LevelParser
