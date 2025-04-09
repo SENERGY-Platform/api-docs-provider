b6ee00a9c9bc0c21bc4702421d5d4c89af56f021
y-du <42994541+y-du@users.noreply.github.com>
2025-04-09 08:21:34 +0200

    init

---
+++ pkg/service/asyncapi_srv/itf.go
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package asyncapi_srv
+
+import (
+	"context"
+	"github.com/SENERGY-Platform/swagger-docs-provider/pkg/models"
+)
+
+type StorageHandler interface {
+	List(ctx context.Context) ([]models.StorageData, error)
+	Write(ctx context.Context, id string, args [][2]string, data []byte) error
+	Read(ctx context.Context, id string) ([]byte, error)
+	Delete(ctx context.Context, id string) error
+}
